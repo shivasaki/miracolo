b27ecf19f88f6deb4558ae3b60d215d44a85c4b4
shivasaki <shivasaki0329@gmail.com>
2017-06-05 09:46:36 +0900

    jsをpublicに置いて機能

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -2,7 +2,7 @@ module ContentsHelper
     def set_content(user_id, place_id)
         @content = Content.find_by(user_id: user_id, place_id: place_id)
         if @content.nil?
-            @content = Content.create(user_id: user_id)
+            @content = Content.create(title: "TITLE", text: "TEXT", start: Time.zone.now, end: Time.zone.now, user_id: user_id, place_id: 99)
         else
             return @content
         end
