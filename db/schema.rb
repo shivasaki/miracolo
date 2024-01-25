3b0ff1c682f2641e929198b1c85ec5cae9e9b4c0
shivasaki <shivasaki0329@gmail.com>
2024-01-26 03:05:43 +0900

    update rails

--- db/schema.rb
+++ db/schema.rb
@@ -2,34 +2,33 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526011630) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_01_25_174047) do
   create_table "contents", force: :cascade do |t|
-    t.string   "title"
-    t.text     "text"
-    t.datetime "start"
-    t.datetime "end"
-    t.integer  "user_id"
-    t.integer  "place_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "text"
+    t.datetime "start", precision: nil
+    t.datetime "end", precision: nil
+    t.integer "user_id"
+    t.integer "place_id"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.string "password_digest"
   end
 
 end
